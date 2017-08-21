d30444b2c3b0578616bab121d69560abe1483755
310220454 <310220454@YY012842.code1.emi.philips.com>
2017-08-21 19:41:36 +0530

    First Commit

---
+++ Mav3/src/main/java/mars/Demo.java
@@ -0,0 +1,26 @@
+package mars;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+@Test
+public void test()
+{
+	System.out.println("hello world");
+	
+	//WebDriver driver = new FirefoxDriver();
+	
+	
+	
+
+	
+}
+
+
+}
